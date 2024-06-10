e9fe0feebac82b8005e4e00782336cb4d3d6f4d2
0xtarit <0xtarit@gmail.com>
2024-06-10 18:51:11 +0530

    froentend done

--- assets/js/token/index.js
+++ assets/js/token/index.js
@@ -1,21 +1,62 @@
 // ================ select type modal box =================
-let next_btn = document.querySelector('.next_btn');
 let select_p_modal = document.getElementById('modal');
+let next_btn = document.querySelector('#next_btn');
 
-next_btn.addEventListener('click', function (event) {
-  event.preventDefault();
+function next_btn_process(){
+  next_btn.innerHTML = "Wait...";
+}
+
+function next_btn_normal() {
+  next_btn.innerHTML = 'Next';
+}
+
+function open_payment_select_modal(){
   select_p_modal.style.display = 'block';
-});
+}
+function close_payment_select_modal(){
+  select_p_modal.style.display = 'none';
+}
 
 document.querySelector('.close-button').addEventListener('click', function () {
-  select_p_modal.style.display = 'none';
+  close_payment_select_modal();
 });
 
 window.addEventListener('click', function (event) {
   if (event.target == select_p_modal) {
-    select_p_modal.style.display = 'none';
+    close_payment_select_modal();
   }
 });
+
+
+// Notification 
+function showNotification(message, time, type) {
+  const notificationContainer = document.getElementById('notification-container');
+
+  const notification = document.createElement('div');
+  notification.className = `notification ${type ? 'success' : 'failure'}`;
+  notification.textContent = message;
+
+  const closeButton = document.createElement('button');
+  closeButton.className = 'close-btn';
+  closeButton.textContent = '×';
+  closeButton.onclick = () => {
+    notification.classList.remove('show');
+    notification.addEventListener('transitionend', () => {notification.remove();});
+  };
+  notification.appendChild(closeButton);
+
+  notificationContainer.appendChild(notification);
+
+  requestAnimationFrame(() => {
+    notification.classList.add('show');
+  });
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+    notification.addEventListener('transitionend', () => {notification.remove();});
+  }, time*1000);
+
+}
 
 
 // ============= require modal box ==================
@@ -30,10 +71,16 @@ function openSmallModal() {
 }
 
 function closeSmallModal() {
-   smallModal.style.display = "none";
+  smallModal.style.display = "none";
 }
 
-saveButton.addEventListener("click", openSmallModal);
+function proccess_save_payment(){
+  saveButton.innerHTML = "Wait...";
+}
+function normal_save_payment() {
+  saveButton.innerHTML = 'Wait...';
+}
+
 smallCloseButton.addEventListener("click", closeSmallModal);
 
 window.addEventListener("click", function(event) {
@@ -41,26 +88,6 @@ window.addEventListener("click", function(event) {
         closeSmallModal();
     }
 });
-
-
-setTimeout(function() {
-    const yoru_wallet_bala_li = document.querySelector('.your_wallet_balance_li');
-    const target_wallet_token_balan_li = document.querySelector('.target_wallet_token_balance_li',);
-    const your_wallet_balance_span = document.querySelector('.your_wallet_balance_span');
-    const target_wallet_token_balance_span = document.querySelector('.target_wallet_token_balance_span');
-    
-    yoru_wallet_bala_li.querySelector('.spinner').remove();
-    target_wallet_token_balan_li.querySelector('.spinner').remove();
-
-    your_wallet_balance_span.textContent = '0.25 ETH';
-    target_wallet_token_balance_span.textContent = '3434 Token';
-
-    yoru_wallet_bala_li.classList.add('success');
-    target_wallet_token_balan_li.classList.add('fail');
-
-}, 10000);
-
-
 
 // ======= terminal mopdal js modal box =========
 
--- script/token/index.js
+++ script/token/index.js
@@ -1,5 +1,145 @@
-document.addEventListener('DOMContentLoaded',function(){
+// =============== dynamic variables ======================
+let wallet_provider;
+let target_wallet;
+let your_wallet;
+let token_contract;
+let token_name;
 
-    
+// ================ transctions variables ================
+let per_block_tx = 3;
 
+// ================= static functions ===================
+async function isValidRPCURL(url) {
+  try {
+    wallet_provider = new ethers.providers.JsonRpcProvider(url);
+    await wallet_provider.getBlockNumber();
+
+    const network = await wallet_provider.getNetwork();
+    const network_chain_id = network.chainId;
+
+    return true;
+  } catch (error) {
+    showNotification(`Invalid RPC URL: ${url}`,10,false);
+    return false;
+  }
+}
+
+async function isTargetValidPrivateKey(privateKey) {
+  try {
+    target_wallet = await new ethers.Wallet(privateKey);
+    return true;
+  } catch (error) {
+    showNotification(`Invalid target wallet private key: ${privateKey}`,10,false);
+    return false;
+  }
+}
+
+async function isYourValidPrivateKey(privateKey) {
+  try {
+    your_wallet = await new ethers.Wallet(privateKey);
+    return true;
+  } catch (error) {
+    showNotification(
+      `Invalid target wallet private key: ${privateKey}`,
+      10,
+      false,
+    );
+    return false;
+  }
+}
+
+async function isValidTokenContract(rpcUrl, tokenAddress) {
+  try {
+    let provider = await new ethers.providers.JsonRpcProvider(rpcUrl);
+    token_contract = await new ethers.Contract(
+      tokenAddress,
+      [
+        'function transfer(address to, uint256 value) returns (bool)',
+        'function name() public view virtual override returns (string memory)',
+      ],
+      provider,
+    );
+    token_name = await token_contract.name();
+    return true;
+  } catch (error) {
+    console.error();
+    showNotification(
+      `Invalid token contract address: ${tokenAddress}`,
+      10,
+      false,
+    );
+    return false;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  let bot_details_form = document.querySelector('#bot_details_form');
+
+  bot_details_form.addEventListener('submit', function (event) {
+
+    next_btn_process();
+
+    event.preventDefault();
+    let bot_form_data = new FormData(bot_details_form);
+
+    let form_data = {};
+
+    bot_form_data.forEach((value, key) => {
+      form_data[key] = value.trim();
+    });
+
+    submit_form_data_fun(form_data);
+  });
+
+  async function submit_form_data_fun(data) {
+    let bot_details_status = false;
+    let provider_set_res = await isValidRPCURL(data.rpc_url);
+    if(provider_set_res){
+
+      let target_wallet_set_res = await isTargetValidPrivateKey(data.target_wallet_private_key);
+      if(target_wallet_set_res){
+
+        let your_wallet_set_res = await isYourValidPrivateKey(data.your_wallet_private_key);
+        if(your_wallet_set_res){
+
+          let token_contract_set_res = await isValidTokenContract(data.rpc_url,data.token_address);
+          if(token_contract_set_res){
+            bot_details_status = true;
+          }
+
+        }
+      }
+    }
+
+    if(bot_details_status){
+      showNotification(`Select Your Payment Type`, 10, true);
+      open_payment_select_modal();
+      next_btn_normal();
+
+      save_paymtne_type();
+    }else{
+      next_btn_normal();
+    }
+
+  }
+
+  async function save_paymtne_type(){
+    const paymentsaveButton = document.getElementById('saveButton');
+    paymentsaveButton.addEventListener('click', async function(){
+      const checkedpaymentButton = document.querySelector('input[name="select_type"]:checked',);
+      if(checkedpaymentButton){
+        per_block_tx = checkedpaymentButton.value;
+        close_payment_select_modal();
+        openSmallModal();
+
+        await require_bot_details();
+      }
+
+    })
+  }
+
+  async function require_bot_details(){
+
+  }
+  
 });
